2c702ea31b43e9ac6635deafa9d0ffcd694b7722
KaraKala1427 <47604214+KaraKala1427@users.noreply.github.com>
2019-06-10 22:07:19 +0600

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,30 +1,34 @@
 import os
 from telegram.ext import Updater, ConversationHandler, CommandHandler, MessageHandler, Filters
 
-INITIAL, MIDDLE, FINAL = range(3)
-
+INITIAl, MIDDLE, FINAL = range(3)
 
 def start(bot, updater):
     updater.message.reply_text('Hello')
-    return INITIAL
+    return INITIAl
 
 
 def say_hello(bot, updater):
-    updater.message.reply_text('INITIAL')
+    updater.message.reply_text('I am in INITIAL state')
     return MIDDLE
 
 
 def say_howdy(bot, updater):
-    updater.message.reply_text('MIDDLE')
+    updater.message.reply_text('I am in MIDDLE state')
     return FINAL
 
 
-def say_goodbye(bot, updater):
-    updater.message.reply_text('FINAL')
+def say_good_bye(bot, updater):
+    updater.message.reply_text('I am in FINAL state, goodbye')
 
 
 def cancel(bot, updater):
-    updater.message.reply_text('good bye')
+    updater.message.reply_text('Good bye')
+    return ConversationHandler.END
+
+
+def help(bot, updater):
+    updater.message.reply_text('Bot started')
 
 
 def main():
@@ -33,21 +37,23 @@ def main():
     updater = Updater(TOKEN)
 
     dispatcher = updater.dispatcher
+    command = CommandHandler('help', help)
+
     conv_handler = ConversationHandler(
-        entry_points = [CommandHandler('start', start)],
-        states = {INITIAL: [MessageHandler(Filters.text, say_hello), CommandHandler('init',  start)],
+        entry_points=[CommandHandler('start', start)],
+        states= { INITIAl: [MessageHandler(Filters.text, say_hello), CommandHandler('init', start)],
                   MIDDLE: [MessageHandler(Filters.text, say_howdy)],
-                  FINAL: [MessageHandler(Filters.text, say_goodbye)]},
+                  FINAL: [MessageHandler(Filters.text, say_good_bye)]},
         fallbacks= [CommandHandler('cancel', cancel)]
     )
-    dispatcher.add_handler(conv_handler)
 
+    dispatcher.add_handler(conv_handler)
+    dispatcher.add_handler(command)
+
+    # updater.start_polling()
     updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
     updater.bot.set_webhook("https://yera1235.herokuapp.com/" + TOKEN)
     updater.idle()
-
-    # updater.idle()
-    # updater.start.webhook()
 
 if __name__ == '__main__':
     main()
